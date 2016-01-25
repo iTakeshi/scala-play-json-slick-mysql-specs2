4959f20db5f96a0640f4d73bb885e51cc3a4c12d
iTakeshi <takeshi.ito.doraemon@gmail.com>
2016-01-26 01:09:44 +0900

    controllers should depend on services, but not models

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,10 @@ package controllers
 import play.api.mvc._
 import play.api.libs.json._
 
-import models.{Country, Countries}
+import services.CountryService
 
 class Application extends Controller {
   def index = Action {
-    Ok(Json.toJson(Countries.listAll))
+    Ok(Json.toJson(CountryService.listAllCountries))
   }
 }
